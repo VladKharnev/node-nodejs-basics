e2d54b4a505fee7adf52ee8ab2d3c698b479342b
VladKharnev <slayet2010@yandex.ru>
2022-12-02 15:13:19 +0300

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import path from "node:path";
+import  fs from "node:fs";
+import url from "node:url";
+
 const create = async () => {
-    // Write your code here 
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+  fs.stat(filePath, function(err, stat) {
+    if(err == null) {
+        console.log('FS operation failed');
+    } else if(err.code == 'ENOENT') {
+        fs.writeFile(filePath, "I am fresh and young", (err) => {
+            if (err) {
+              throw err;
+            }
+            console.log("создан");
+          });
+    } else {
+        console.log('Some other error: ', err.code);
+    }
+});
 };
 
 await create();
