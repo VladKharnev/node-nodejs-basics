82813d06b96bee3e3c61e509c56e2dd459e12c27
VladKharnev <slayet2010@yandex.ru>
2022-12-03 13:58:21 +0300

    feat: add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import fs from "node:fs";
 const read = async () => {
-    // Write your code here 
+  fs.stat("./src/fs/files/fileToRead.txt", function (err) {
+    if (!err) {
+        fs.readFile("./src/fs/files/fileToRead.txt", "utf8", 
+        function(error,data){
+            if(error) throw error;
+            console.log(data);
+});
+    } else if (err.code === "ENOENT") {
+      console.log("FS operation failed");
+    }
+  });
 };
-
 await read();
