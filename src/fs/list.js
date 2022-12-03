3e47b4ea71624af83eb3637a80136aca29e56aa0
VladKharnev <slayet2010@yandex.ru>
2022-12-03 13:52:33 +0300

    feat: add list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from "node:fs";
 const list = async () => {
-    // Write your code here 
+  fs.stat("./src/fs/files", function (err) {
+    if (!err) {
+        fs.readdir('./src/fs/files', (err, data)=>{
+            data.forEach(file => {
+                console.log(file)
+            })
+        })
+    } else if (err.code === "ENOENT") {
+      console.log("FS operation failed");
+    }
+  });
 };
 
 await list();
