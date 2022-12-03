07db66611f8de55f0d0893e8251e96a5c8b12c16
VladKharnev <slayet2010@yandex.ru>
2022-12-03 13:37:46 +0300

    feat: add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
-const remove = async () => {
-    // Write your code here 
-};
+import fs from "node:fs";
 
+const remove = async () => {
+  fs.stat("./src/fs/files/fileToRemove.txt", function (err) {
+    if (!err) {
+      fs.unlink("./src/fs/files/fileToRemove.txt", (err) => {
+        if (err) throw err; // не удалось удалить файл
+        console.log("Файл успешно удалён");
+      });
+    } else {
+      console.log("FS operation failed");
+    }
+  });
+};
 await remove();
