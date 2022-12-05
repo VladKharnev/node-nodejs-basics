441d309a2d33588a2c58a38cbaf04c8d19585224
VladKharnev <slayet2010@yandex.ru>
2022-12-05 15:23:18 +0300

    feat: add wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import {Worker} from 'worker_threads';
+import os from 'node:os';
+import {fileURLToPath} from "node:url";
+import path, {dirname} from "node:path";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
+const performCalculations = async () => {
+
+    const proc = os.cpus()
+    let numb = 10
+    const results = await Promise.allSettled(proc.map(() => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(path.join(__dirname, '/worker.js'), {
+                workerData: numb++
+            })
+            worker.on('message', msg => resolve(msg))
+            worker.on('error', msg => reject(msg))
+        })
+    }))
+    const final = results.map(({status, value}) => ({
+        status: status === "fulfilled" ? 'resolved' : 'error',
+        data: status === 'fulfilled' ? value : null
+    }))
+
+    console.log(final);
+    return final;
+}
 await performCalculations();
